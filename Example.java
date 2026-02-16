b314b8133d51977d0e5a6253ea634772ca5dc96e
Mohanakrupa08 <samamohanakrupa@gmail.com>
2026-02-16 19:08:49 +0530

    Update Example.java
    updated

--- Example.java
+++ Example.java
@@ -1,8 +1,10 @@
 class Example{
     public static void main(String[] args){
         int num1=5;
+        
         int num2=4;
         int result=num1+num2;
         System.out.println(result);
     }
+
 }
